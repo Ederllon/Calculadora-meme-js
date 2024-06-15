5ee93d9b40715335781016c07f940759a7f513a4
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-14 23:24:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+const btn = document.getElementById("btn");
+
+    
+
+$(document).ready(()=>{
+     function cleiton(){
+         $("#img").show(2500);
+          $(".texto").show(2500);
+         
+     }
+    $("#img").hide(0);
+    $(".texto").hide(0);
+    $("#btn").click(function(){
+        cleiton();
+        
+    });
+    
+    
+    
+    
+})
+
